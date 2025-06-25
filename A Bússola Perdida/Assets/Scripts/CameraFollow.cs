23c52a893c48cc0dcdf655aea4192790ebc9c3b0
Samyta Ramiza <145566706+SamytaS2@users.noreply.github.com>
2025-06-24 21:09:06 -0300

    cameraFollow

--- A Bússola Perdida/Assets/Scripts/CameraFollow.cs
+++ A Bússola Perdida/Assets/Scripts/CameraFollow.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Camera : MonoBehaviour
+public class CameraFollow : MonoBehaviour
 {
     public Transform alvo;
     public Vector3 offset;
