c98eda7bd9f5ebce966fb2b0c7423f8d572b761a
Samyta Ramiza <145566706+SamytaS2@users.noreply.github.com>
2025-08-14 18:17:03 -0300

    Tela de vitória no canvas

--- A Bússola Perdida/Assets/Scripts/SoundManager.cs
+++ A Bússola Perdida/Assets/Scripts/SoundManager.cs
@@ -3,16 +3,40 @@ using UnityEngine.UI;
 
 public class SoundManager : MonoBehaviour
 {
+    //SerializeFiel - se usa quando o atribute deve ser privado, mas aparacer no editor
     [SerializeField] Slider VolumeSlider;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        if(!PlayerPrefs.HasKey("musicVolume"))
+        {
+            PlayerPrefs.SetFloat("musicVolume", 1);
+            Load();
+        }
+        else
+        {
+            Load();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void ChangeVolume()
     {
-        
+        //volume do jogo será igual ao do controle deslizante
+        AudioListener.volume = VolumeSlider.value;
+        Save();
     }
+
+    private void Load()
+    {
+        //garante que o valor do controle deslizante seja igual ao armazenado
+        VolumeSlider.value = PlayerPrefs.GetFloat("musicVolume");
+    }
+
+    private void Save()
+    {
+        //armazena o valor do volume
+        PlayerPrefs.SetFloat("musicVolume", VolumeSlider.value);
+    }
+
 }
