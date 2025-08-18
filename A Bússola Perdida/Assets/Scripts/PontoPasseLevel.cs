d64a8a86f50ef5f6aaee25f1a4f525210461518c
Samyta Ramiza <145566706+SamytaS2@users.noreply.github.com>
2025-08-18 19:41:32 -0300

    canvas corrigido

--- A Bússola Perdida/Assets/Scripts/PontoPasseLevel.cs
+++ A Bússola Perdida/Assets/Scripts/PontoPasseLevel.cs
@@ -1,16 +1,23 @@
-using UnityEngine; // Biblioteca principal do Unity (acesso a componentes, objetos, etc.)
 using UnityEngine.SceneManagement; // Necessária para trocar de cenas no jogo
+using UnityEngine.UI;
+using UnityEngine; // Biblioteca principal do Unity (acesso a componentes, objetos, etc.)
+
 
 public class PontoPasseLevel : MonoBehaviour
 {
-    public string lvlName;
+    //
+    public string nextLvlName;
 
     // Definida como TRUE na última fase
     public bool ultimaFase = false;
 
     // Referência ao painel/canvas com a mensagem de vitória
     public GameObject PanelVictory; 
+    public GameObject scorePocao;
+    public GameObject scoreRosa;
 
+    public Image imgPocao;
+    public Image imgRosa;
 
     // Chamado automaticamente quando outro Collider2D entra na área marcada como "Is Trigger"
     void OnTriggerEnter2D(Collider2D collider)
@@ -22,16 +29,27 @@ public class PontoPasseLevel : MonoBehaviour
             if (ultimaFase)
             {
                 // Ativa o painel de vitória, se ele estiver configurado no Inspector
-                if (PanelVictory != null)
+                if (PanelVictory != null){
                     PanelVictory.SetActive(true);
-                
+                }
+
+                if(imgPocao != null && imgRosa != null){
+                    imgPocao.enabled = false;
+                    imgRosa.enabled = false;
+                }
+
+                if(scorePocao && scoreRosa != null){
+                    scorePocao.SetActive(false);
+                    scoreRosa.SetActive(false);
+                }
+
                 // Pausa o jogo para o jogador poder ver a mensagem sem que a ação continue
                 Time.timeScale = 0f;
             }
             else
             {
                 // Caso não seja a última fase, carrega a próxima cena
-                SceneManager.LoadScene(lvlName);
+                SceneManager.LoadScene(nextLvlName);
             }
         }
     }
